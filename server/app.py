9ec1f5e462120a82d57680d1797e030e3a69e8dc
Lucas Von der Heyde <lucasvonderheyde@gmail.com>
2023-10-25 07:14:58 -0400

    finished lab

--- server/app.py
+++ server/app.py
@@ -1,8 +1,43 @@
-#!/usr/bin/env python3
 
-from flask import Flask
+from flask import Flask, make_response
 
 app = Flask(__name__)
+
+@app.route("/")
+def index_route():
+    return make_response('<h1>Python Operations with Flask Routing and Views</h1>', 200)
+
+@app.route('/print/<parameter>')
+def print_route(parameter):
+    print(parameter)
+    return make_response(parameter, 200)
+
+@app.route('/count/<parameter>')
+def count_route(parameter):
+    count = ''
+    for i in range(0, int(parameter)):
+        count += str(i)
+        count += "\n"
+    print(count)
+    return make_response(count, 200)
+
+@app.route('/math/<number1>/<operation>/<number2>')
+def math_route(number1, operation, number2):
+    result = 0
+    print(operation)
+    if operation == '*':
+        result = int(number1) * int(number2)
+    elif operation == '+':
+        result = int(number1) + int(number2)
+    elif operation == '-':
+         result = int(number1) - int(number2)
+    elif operation == '/':
+        result = int(number1) / int(number2)
+    elif operation == 'div':
+        result = int(number1) / int(number2)
+    
+    return make_response(str(result), 200)
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
